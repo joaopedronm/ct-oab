e63b0feff47e54fb10fdeb5247b574bd47b2d0de
joaopedronm <joaopedron253@gmail.com>
2025-04-28 22:45:22 -0300

    criação da bio

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+// SCROLL REVEAL
+
+ScrollReveal().reveal('#home', {
+    origin: 'left',
+    duration: 2000,
+    // distance: '20%'
+})
+
+ScrollReveal().reveal('#about', {
+    origin: 'right',
+    duration: 2000,
+    // distance: '20%'
+})
+
+ScrollReveal().reveal('#skills', {
+    origin: 'left',
+    duration: 2000,
+    // distance: '20%'
+})
+
+ScrollReveal().reveal('#qualifications', {
+    origin: 'right',
+    duration: 2000,
+    // distance: '20%'
+})
+
+ScrollReveal().reveal('#contact', {
+    origin: 'left',
+    duration: 2000,
+    // distance: '20%'
+})
